35c532d5e698bac367ee921da6466cc9ef8c8ac8
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2020-02-05 14:55:51 -0400

    Contexto

---
+++ PrimerParcialAP2/Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAP2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server =.\SqlExpress; Database= PrimerParcialDb; Trusted_Connection= True;");
+        }
+
+
+
+
+    }
+}
